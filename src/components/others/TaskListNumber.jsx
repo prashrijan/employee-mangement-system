615c91dbeadb7ac3a16044b5b1722fdf98ef8c35
Prashrijan <shresthaprashrijan@gmail.com>
2025-01-13 13:57:55 +1100

    task list scroll bar design

--- src/components/others/TaskListNumber.jsx
+++ src/components/others/TaskListNumber.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const TaskListNumber = ({ data }) => {
   console.log(data);
   return (
-    <div className="flex mt-10 justify-between gap-5 w-full">
+    <div className="flex mt-10 justify-between gap-5 w-full flex-wrap md:flex-nowrap">
       <div className="w-[45%] py-6 px-9 bg-blue-400 rounded-xl">
         <h2 className="text-3xl font-semibold">{data.taskNumbers?.newTask}</h2>
         <h3 className="text-xl font-medium">New Task</h3>
